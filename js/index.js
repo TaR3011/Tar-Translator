0541662ed00d57052c24cd6e046fe633ec87e7b8
trr <trrbader@gmail.com>
2024-05-25 03:47:57 +0300

    compelte fetching translated text and exchange between text with copy and speech feature'

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 const selectTagEl = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchange = document.querySelector(".exchange");
+const translateBtn = document.querySelector("button");
+const icons = document.querySelectorAll(".row i");
 let option = "";
 
 /////// to set English selected to first selection as default
@@ -49,3 +54,58 @@ selectTagEl.forEach((t, id) => {
     t.insertAdjacentHTML("beforeend", option);
   }
 });
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value;
+  let trnslateFrom = selectTagEl[0].value;
+  let translateTo = selectTagEl[1].value;
+
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${trnslateFrom}|${translateTo}`;
+
+  if (!text) return;
+  toText.setAttribute("placeholder", "Translating...");
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let newText = data["responseData"].translatedText;
+      toText.value = newText;
+      toText.setAttribute("placeholder", "Translation");
+    });
+});
+
+exchange.addEventListener("click", () => {
+  exchangeFun([fromText, toText, selectTagEl[0], selectTagEl[1]]);
+});
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("fa-copy")) {
+      if (target.id === "from") {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(toText.value);
+      }
+    } else {
+      let utterance;
+      if (target.id === "from") {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTagEl[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(toText.value);
+        utterance.lang = selectTagEl[1].value;
+      }
+      speechSynthesis.speak(utterance); // speak the passed utterance
+    }
+  });
+});
+
+// function to chgane between from and to text with its slections
+let exchangeFun = ([fisrtText, secondText, firstSelect, secondSelect]) => {
+  let tempText = fisrtText.value;
+  fisrtText.value = secondText.value;
+  secondText.value = tempText;
+
+  let tempSelectTag = firstSelect.value;
+  firstSelect.value = secondSelect.value;
+  secondSelect.value = tempSelectTag;
+};
