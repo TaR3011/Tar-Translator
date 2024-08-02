3cc16ac38e50d4d28eae043f3ed5ba1aacb119b6
trr <trrbader@gmail.com>
2024-08-02 11:01:50 +0300

    make the app responsive

--- js/index.js
+++ js/index.js
@@ -6,41 +6,6 @@ const translateBtn = document.querySelector("button");
 const icons = document.querySelectorAll(".row i");
 let option = "";
 
-/////// to set English selected to first selection as default
-// for (country_code in countries) {
-//   let selected;
-//   if (
-//     country_code === "en-GB" &&
-//     selectTagEl[0].getAttribute("id") === "from"
-//   ) {
-//     selected = "selected";
-//   } else if (
-//     country_code === "ar-SA" &&
-//     selectTagEl[1].getAttribute("id") === "to"
-//   ) {
-//     selected = "selected";
-//   } else {
-//     selected = "none";
-//   }
-//   option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
-//   selectTagEl[0].insertAdjacentHTML("beforeend", option);
-// }
-/////// to set arabic selected to second selection as default
-// for (country_code in countries) {
-//   let selected;
-//   if (country_code === "ar-SA" && selectTagEl[1].getAttribute("id") === "to") {
-//     selected = "selected";
-//   } else if (
-//     country_code === "en-GB" &&
-//     selectTagEl[0].getAttribute("id") === "form"
-//   ) {
-//     selected = "selected";
-//   } else {
-//     selected = "none";
-//   }
-//   option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
-//   selectTagEl[1].insertAdjacentHTML("beforeend", option);
-// }
 
 selectTagEl.forEach((t, id) => {
   for (const country_code in countries) {
